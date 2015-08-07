8a23be7b0f917b4cc9ec0eb152d5c0e06f623388
Andy Shaw <shicang1990@gmail.com>
2015-08-07 16:31:45 +0800

    Update: 1.新增API:循环滚动开关 2.动态创建滚动显示视图，减小内存占用

--- Classes/SCLoopScrollView.m
+++ Classes/SCLoopScrollView.m
@@ -19,8 +19,7 @@
 
 typedef void(^BLOCK)(NSInteger index);
 
-@implementation SCLoopScrollView
-{
+@implementation SCLoopScrollView {
     BLOCK          _scrollBlock;
     BLOCK          _tapBlock;
     
@@ -32,76 +31,71 @@ typedef void(^BLOCK)(NSInteger index);
 }
 
 #pragma mark - Init Methods
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
+- (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
-    if (self)
-    {
+    if (self) {
         [self initConfig];
     }
     return self;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    if (self)
-    {
+    if (self) {
         [self initConfig];
     }
     return self;
 }
 
 #pragma mark - Config Methods
-- (void)initConfig
-{
+- (void)initConfig {
+    _loopScroll = YES;
     _manager = [[SCLoopManager alloc] init];
 }
 
-- (void)viewConfig
-{
+- (void)viewConfig {
     [self layoutIfNeeded];
+    
+    NSInteger imageCount = _images.count;
     // 初始化并配置ScrollView以及其三个子视图ImageView，刷新三个ImageView并显示Image
-    if (!_scrollView)
-    {
+    if (!_scrollView) {
         _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(ZERO_POINT, ZERO_POINT, SELF_WIDTH, SELF_HEIGHT)];
         _scrollView.delegate = self;
         _scrollView.pagingEnabled = YES;
+        _scrollView.scrollEnabled = imageCount >> 1;
         _scrollView.showsHorizontalScrollIndicator = NO;
         _scrollView.showsVerticalScrollIndicator = NO;
         _scrollView.contentSize = CGSizeMake(SELF_WIDTH * 3, ZERO_POINT);
         [_scrollView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGestureRecognizer)]];
         [self addSubview:_scrollView];
     }
-    if (!_firstImageView)
-    {
+    if (!_firstImageView && (imageCount > 1)) {
         _firstImageView = [[UIImageView alloc] initWithFrame:CGRectMake(ZERO_POINT, ZERO_POINT, SELF_WIDTH, SELF_HEIGHT)];
         [_scrollView addSubview:_firstImageView];
     }
-    if (!_centerImageView)
-    {
-        _centerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(SELF_WIDTH, ZERO_POINT, SELF_WIDTH, SELF_HEIGHT)];
+    if (!_centerImageView) {
+        _centerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(((imageCount > 1) ? SELF_WIDTH : ZERO_POINT), ZERO_POINT, SELF_WIDTH, SELF_HEIGHT)];
         [_scrollView addSubview:_centerImageView];
     }
-    if (!_lastImageView)
-    {
+    if (!_lastImageView && (imageCount > 1)) {
         _lastImageView = [[UIImageView alloc] initWithFrame:CGRectMake(SELF_WIDTH*2, ZERO_POINT, SELF_WIDTH, SELF_HEIGHT)];
         [_scrollView addSubview:_lastImageView];
     }
-    _scrollView.scrollEnabled = _images.count >> 1;
     [self display];
 }
 
 #pragma mark - Setter And Getter Methods
-- (NSInteger)index
-{
+- (void)setLoopScroll:(BOOL)loopScroll {
+    _loopScroll = loopScroll;
+    [_manager canLoop:loopScroll];
+}
+
+- (NSInteger)index {
     return _manager.currentItem.index;
 }
 
-- (void)setImages:(NSArray *)images
-{
-    if (images.count)
-    {
+- (void)setImages:(NSArray *)images {
+    if (images.count) {
         _images = images;
         _manager.images = images;
     }
@@ -109,15 +103,13 @@ typedef void(^BLOCK)(NSInteger index);
 
 #pragma mark - Public Methods
 - (void)show:(void(^)(NSInteger index))tap
-    finished:(void(^)(NSInteger index))finished
-{
+    finished:(void(^)(NSInteger index))finished {
     [self showWithAutoScroll:NO tap:tap finished:finished];
 }
 
 - (void)showWithAutoScroll:(BOOL)autoScroll
                        tap:(void(^)(NSInteger index))tap
-                  finished:(void(^)(NSInteger index))finished
-{
+                  finished:(void(^)(NSInteger index))finished {
     _autoScroll  = autoScroll;
     _tapBlock    = tap;
     _scrollBlock = finished;
@@ -126,16 +118,14 @@ typedef void(^BLOCK)(NSInteger index);
 }
 
 #pragma mark - Private Methods
-- (void)display
-{
+- (void)display {
     [self handelInitConfigImageView:_firstImageView];
     [self handelInitConfigImageView:_centerImageView];
     [self handelInitConfigImageView:_lastImageView];
     [self refreshImage];
 }
 
-- (void)handelInitConfigImageView:(UIImageView *)imageView
-{
+- (void)handelInitConfigImageView:(UIImageView *)imageView {
     imageView.backgroundColor = _defaultImage ? [UIColor clearColor] : [UIColor lightGrayColor];
     imageView.image = _defaultImage;
 }
@@ -143,43 +133,41 @@ typedef void(^BLOCK)(NSInteger index);
 /**
  *  单击事件
  */
-- (void)tapGestureRecognizer
-{
-    if (_tapBlock)
+- (void)tapGestureRecognizer {
+    if (_tapBlock) {
         _tapBlock(_manager.currentItem.index);
+    }
 }
 
 /**
  *  左滑事件
  */
-- (void)swipeLeft
-{
+- (void)swipeLeft {
     [_manager moveRight];
     [self refreshImage];
-    if (_scrollBlock)
+    if (_scrollBlock) {
         _scrollBlock(_manager.currentItem.index);
+    }
 }
 
 /**
  *  右滑事件
  */
-- (void)swipeRight
-{
+- (void)swipeRight {
     [_manager moveLeft];
     [self refreshImage];
-    if (_scrollBlock)
+    if (_scrollBlock) {
         _scrollBlock(_manager.currentItem.index);
+    }
 }
 
 /**
  *  刷新三个子视图所显示的Image
  */
-- (void)refreshImage
-{
+- (void)refreshImage {
     [_manager.currentItem request:^(SCLoopItem *item) {
         _centerImageView.image = [UIImage imageWithData:item.data];
-        if (_images.count > 1)
-        {
+        if (_images.count > 1) {
             [item.preItem request:^(SCLoopItem *item) {
                 _firstImageView.image = [UIImage imageWithData:item.data];
             }];
@@ -194,33 +182,32 @@ typedef void(^BLOCK)(NSInteger index);
 /**
  *  重设ScrollView偏移位置
  */
-- (void)resetOffset
-{
+- (void)resetOffset {
     _scrollView.contentOffset = CGPointMake(SELF_WIDTH, ZERO_POINT);
 }
 
 #pragma mark - UISrollView Delegate Methods
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     CGFloat offsetX = scrollView.contentOffset.x;
-    if (offsetX <= MIN_BORDER)
+    if (offsetX <= MIN_BORDER) {
         [self swipeRight];
-    else if (offsetX >= MAX_BORDER)
+    } else if (offsetX >= MAX_BORDER) {
         [self swipeLeft];
+    }
 }
 
-- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
-{
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
     // 防止滚动结束后的卡顿处理
-    if (scrollView.contentOffset.x/SELF_WIDTH)
+    if (scrollView.contentOffset.x/SELF_WIDTH) {
         [self resetOffset];
+    }
 }
 
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
-{
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     // 防止滚动结束后的卡顿处理
-    if (scrollView.contentOffset.x/SELF_WIDTH)
+    if (scrollView.contentOffset.x/SELF_WIDTH) {
         [self resetOffset];
+    }
 }
 
 @end

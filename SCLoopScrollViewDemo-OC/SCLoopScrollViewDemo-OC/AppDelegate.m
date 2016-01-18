d9b6a66569c00c09d908513f3b6dfb1179ed4ce9
RockerHX <RockerHX@gmail.com>
2016-01-18 17:43:40 +0800

    Update

--- SCLoopScrollViewDemo-OC/SCLoopScrollViewDemo-OC/AppDelegate.m
+++ SCLoopScrollViewDemo-OC/SCLoopScrollViewDemo-OC/AppDelegate.m
@@ -9,15 +9,9 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
--- SCLoopScrollViewDemo-OC/SCLoopScrollViewDemo-OC/ViewController.m
+++ SCLoopScrollViewDemo-OC/SCLoopScrollViewDemo-OC/ViewController.m
@@ -23,19 +23,18 @@
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
-    NSArray *images = @[@"http://e.hiphotos.baidu.com/image/w%3D310/sign=429b5adbbb014a90813e40bc99763971/622762d0f703918ffc60d8a3533d269759eec422.jpg",
+    NSArray *images = @[@"http://i8.tietuku.com/a6542887e864efc0.jpg",
                         [UIImage imageNamed:@"0"],
-                        @"http://g.hiphotos.baidu.com/image/w%3D310/sign=9b62b9f379899e51788e3c1572a6d990/8718367adab44aed80ebd4eab11c8701a18bfb13.jpg",
+                        @"http://i8.tietuku.com/5730f7ebadae2c41.jpg",
                         [UIImage imageNamed:@"1"],
-                        @"http://f.hiphotos.baidu.com/image/w%3D310/sign=544d5da6324e251fe2f7e2f99787c9c2/0824ab18972bd40720832c6179899e510eb309e1.jpg",
-                        @"http://www.huabian.com/uploadfile/2014/1008/20141008103712898.jpg"];
+                        @"http://i8.tietuku.com/149caebe062fc98c.jpg"];
     
     SCLoopScrollView *scrollView = [[SCLoopScrollView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.view.frame.size.width, 300.0f)];
     [self.view addSubview:scrollView];
     scrollView.dataSource = images;
     [scrollView show:^(NSInteger index) {
         NSLog(@"Current Index:%@", @(index));
-    } finished:^(NSInteger index) {
+    } scrolled:^(NSInteger index) {
         NSLog(@"Tap Index:%@", @(index));
     }];
 }
